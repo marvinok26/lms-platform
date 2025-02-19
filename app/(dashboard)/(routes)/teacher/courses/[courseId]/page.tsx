87c0943157938fe993087e63ac21dfb39299ca16
Marvin Okongo <mokongo@payserve.co.ke>
2025-02-19 21:28:29 +0300

    course edit page ui

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,21 +1,66 @@
+import { IconBadge } from "@/components/icons-badge";
 import { db } from "@/lib/db";
+import { getAuth } from "@clerk/nextjs/server";
+import { LayoutDashboard } from "lucide-react";
+import { redirect } from "next/navigation";
 
-const CourseIdPage = ({
+const CourseIdPage = async ({
     params
 }: {
     params: {
         courseId: string
     }
 }) => {
+    // Since you're using clerkMiddleware with auth.protect(), 
+    // we don't need to check userId again here
     const course = await db.course.findUnique({
         where: {
             id: params.courseId
         }
     });
 
+    if (!course) {
+        return redirect("/")
+    }
+
+    const requiredFields = [
+        course.title,
+        course.description,
+        course.imageUrl,
+        course.price,
+        course.categoryId
+    ];
+
+    const totalFields = requiredFields.length;
+    const completedFields = requiredFields.filter(Boolean).length;
+
+    const completionText = `(${completedFields}/${totalFields})`;
+
     return ( 
-        <div>
-            Course Id: {params.courseId}
+        <div className="p-6">
+            <div className="flex items-center justify-between">
+                <div className="flex flex-col gap-y-2">
+                    <h1 className="text-2xl font-semibold">
+                        Course Setup
+                    </h1>
+                    <span className="text-sm text-slate-700">
+                        Complete all fields {completionText}
+                    </span>
+                </div>
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+                <div>
+                    <div className="flex items-center gap-x-2">
+                        <IconBadge icon={LayoutDashboard} />
+                        <h2 className="text-xl">
+                            Customize your course
+                        </h2>
+
+                    </div>
+                </div>
+
+            </div>
         </div>
      );
 }
