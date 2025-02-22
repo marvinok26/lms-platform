b83cf8745d82e652c32dfcac9ed227dcaf9089bc
Marvin Okongo <okongomarvin971@gmail.com>
2025-02-23 01:12:31 +0300

    category form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/image-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/image-form.tsx
@@ -2,15 +2,12 @@
 
 import * as z from "zod";
 import axios from "axios";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
 import { ImageIcon, Pencil, PlusCircle } from "lucide-react";
 import Image from "next/image";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
-import { cn } from "@/lib/utils";
 import { Course } from "@prisma/client";
 
 import { FileUpload } from "@/components/file-upload";
@@ -34,15 +31,6 @@ export const ImageForm = ({ initialData, courseId }: ImageFormProps) => {
   };
 
   const router = useRouter();
-
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      imageUrl: initialData?.imageUrl || "",
-    },
-  });
-
-  const { isSubmitting, isValid } = form.formState;
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -5,6 +5,7 @@ import { redirect } from "next/navigation";
 import { TitleForm } from "./_components/title-form";
 import { DescriptionForm } from "./_components/description-form";
 import { ImageForm } from "./_components/image-form";
+import { CategoryForm } from "./_components/category-form";
 
 const CourseIdPage = async ({
     params
@@ -20,6 +21,14 @@ const CourseIdPage = async ({
             id: params.courseId
         }
     });
+
+    const categories = await db.category.findMany({
+        orderBy: {
+            name: "asc",
+        },
+    });
+
+    console.log(categories)
 
     if (!course) {
         return redirect("/")
@@ -73,6 +82,15 @@ const CourseIdPage = async ({
                         initialData={course}
                         courseId={course.id}
                     />
+
+                    <CategoryForm
+                        initialData={course}
+                        courseId={course.id}
+                        options={categories.map((category) => ({
+                            label: category.name,
+                            value: category.id
+                        }))}
+                    />
                 </div>
 
             </div>
@@ -81,5 +99,3 @@ const CourseIdPage = async ({
 }
 
 export default CourseIdPage;
-
-//email
