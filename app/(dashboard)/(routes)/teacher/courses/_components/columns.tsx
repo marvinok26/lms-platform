2dc75d7f0f524be0979570c23625b94bc435c42b
Marvin Okongo <okongomarvin971@gmail.com>
2025-03-19 22:59:13 +0300

    almost

---
+++ app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import { ColumnDef } from "@tanstack/react-table"
+import { Course } from "@prisma/client"
+import { ArrowUpDown, MoreHorizontal } from "lucide-react"
+import { Button } from "@/components/ui/button"
+
+
+export const columns: ColumnDef<Course>[] = [
+  {
+    accessorKey: "title",
+    header: ({ column }) => {
+        return(
+            <Button 
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            >
+            </Button>
+        )
+    },
+  },
+  {
+    accessorKey: "price",
+    header: "Price",
+  },
+  {
+    accessorKey: "isPublished",
+    header: "Published",
+  },
+
+]
